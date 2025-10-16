50201d5f274345a1013d9929f772be941687d446
Alex Moller <amloelxer@gmail.com>
2025-10-16 10:43:54 -0400

    actually add the project

---
+++ ZestSampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZestSampleProject
+//
+//  Created by Alexander Moller on 10/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ZestSampleProjectTests/ZestSampleProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZestSampleProjectTests.swift
+//  ZestSampleProjectTests
+//
+//  Created by Alexander Moller on 10/16/25.
+//
+
+import Testing
+@testable import ZestSampleProject
+
+struct ZestSampleProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
